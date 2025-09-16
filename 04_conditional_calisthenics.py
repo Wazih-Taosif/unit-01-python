ecd596610503fd113034a9ec5579c99f49ba2676
Wazih-Taosif <haq.wazih@gmail.com>
2025-09-16 10:51:52 -0400

    half done with tasks

---
+++ 04_conditional_calisthenics.py
@@ -0,0 +1,76 @@
+'''
+Exercise 1:
+Check if an integer is greater than 10.
+Return True if conditions are met, False otherwise.
+'''
+print()
+print("---------Task1-------")
+
+integer = 100
+if integer > 10: # checking if integer if greater
+    print("True")
+else:
+    print("False")
+
+'''
+Exercise 2:
+Determine the ticket price based on age and student status.
+Price is $10 if under 18 or a student, $20 otherwise.
+'''
+print()
+print('-------task2-------')
+age = 18
+status = "student" 
+if age < 18 or status == "student": #checking if the person is under 18 or is a student or not.
+    print("Price is $10")
+else:
+    print("Price is $20")#print this if both are false
+
+
+'''
+Exercise 3:
+Prompt the user to enter a fruit name. Check if the fruit is in the list. 
+If it is, print "Yes, that fruit is in the list." 
+If it's not, print "No, that fruit is not in the list."
+'''
+print()
+print('--------task3------')
+task3_list = ["apples", "bananas", "guavas", "papayas", "mangoes"] #list with some fruits name
+fav_fruit = input("What is your favourite fruit? ").strip().lower() #the user types their fav fruit
+if fav_fruit in task3_list:
+    print("Yes, that fruit is in the list.")
+else:
+    print("No, that fruit is not in the list.")
+print()
+
+
+'''
+Exercise 4:
+Check if a year is a century year and a leap year.
+'''
+print()
+
+
+'''
+Exercise 5:
+Calculate the cost of shipping for an online order based on the order weight and destination zone. 
+The shipping cost is $5 per kilogram for Zone A and $7 per kilogram for Zone B. 
+If the order weight is less than 0 kg, return an error message.
+'''
+print()
+print('-------task5-----')
+x = "Zone A"
+y = "Zone B"
+zone = input("Enter the zone :")
+weight = int(input("Enter the weight :"))
+
+if zone == "A" or "Zone A":
+    cost = weight * 5
+    print(f"Shipping cost is {cost}")
+
+
+'''
+Exercise 6:
+Determine the type of a triangle based on side lengths.
+Equilateral, Isosceles, Scalene, or Not a triangle.
+'''
