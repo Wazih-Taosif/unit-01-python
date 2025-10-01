01c162a9b4de7265284131f3474cecc264467b6c
Wazih-Taosif <haq.wazih@gmail.com>
2025-10-01 10:53:11 -0400

    done updating list

--- to-do.py
+++ to-do.py
@@ -1,6 +1,7 @@
-to_do_list = [] #users to do list
 
-
+with open("to_do_list.txt", "r") as file:
+    to_do_list = file.readlines()
+    
 while True:
     count = 0 # resets the list counter to 0. so that every #number of the todo's starts from 1.
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
@@ -12,7 +13,7 @@ while True:
         print(f"{count})", end=" ") #types the number count beside the todo's
         print(x) #prints all the todo's
     print()
-    add_remove_edit_clear = input("Do you want to 'add'/'remove'/'edit' a to-do/ 'clear' all ?" ).lower().strip() #user choose to add/remove/clear/edit a to do.
+    add_remove_edit_clear = input("Do you want to 'add'/'remove'/'edit' a to-do/ 'clear' all/ 'exit' the app ?" ).lower().strip() #user choose to add/remove/clear/edit/exit the app.
     if add_remove_edit_clear == "add":
         adding_todo = input("What is your new to-do? ").strip()
         to_do_list.append(adding_todo) #appends the new todo
@@ -25,5 +26,8 @@ while True:
         to_do_list[editing_todo_index - 1] = edited_newTodo #replaces the existing todo with the new todo, at the same index
     elif add_remove_edit_clear == "clear":
         to_do_list = [] #list is cleared.
-    
+    elif add_remove_edit_clear == "exit":
+        with open("to_do_list.txt", "w") as file:
+            file.write("\n".join(to_do_list)) #writes the new updated list to the text file
+        break
 
