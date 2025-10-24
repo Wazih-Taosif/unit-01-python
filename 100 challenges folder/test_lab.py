ed8c1214980102f1a64e3b981e1f40a8b8d9e370
Wazih-Taosif <haq.wazih@gmail.com>
2025-10-24 10:42:31 -0400

    errors.py

---
+++ 100 challenges folder/test_lab.py
@@ -0,0 +1,11 @@
+"""
+Example 3: List Items
+"""
+
+def get_item(lst, index):
+    item = lst[index]
+    print("Item:", item)
+
+# Example usage:
+my_list = [1, 2, 3]
+get_item(my_list, 5)
--- 14_errors.py
+++ 14_errors.py
@@ -7,33 +7,37 @@ them to include appropriate try/except blocks to handle exceptions gracefully.
 """
 Example 1: Division
 """
-
+print()
+print("--------------Task1----------")
 def divide_numbers(num1, num2):
-    result = num1 / num2
-    print("Result:", result)
-
-# Example usage:
-try:
-    divide_numbers(10, 0)
-except ZeroDivisionError:
-    print("You cannot divide a number by 0. Please make sure the divisor is not 0.")
+    try:
+        result = num1 / num2
+        print("Result:", result)
+    except ZeroDivisionError:
+        print("You cannot divide a number by 0. Please make sure the divisor is not 0.")
+    except TypeError:
+        print("Inputs must be numbers")
+divide_numbers(10, 0)
+divide_numbers(10, 4)
 
 """
 Example 2: Opening Files
 """
 
 def read_file(filename):
-    file = open(filename, 'r')
-    contents = file.read()
-    print("File contents:", contents)
-    file.close()
-try:
-    read_file("nonexistent.txt")
-except FileNotFoundError:
-    import os
-    os.mkdir("nonexistent.txt")
-
-
-
+    try:
+        file = open(filename, 'r')
+        contents = file.read()
+        print("File contents:", contents)
+        file.close()
+    except FileNotFoundError:
+        print("File does not exist.")
 # Example usage:
 read_file("nonexistent.txt")
+        
+
+
+
+
+
+
