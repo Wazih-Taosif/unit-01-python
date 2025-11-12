4a041176f0ad8c9e8d266c69d3dedefb655cc2f6
Wazih-Taosif <haq.wazih@gmail.com>
2025-11-12 10:56:07 -0500

    wroking on wordle

--- 100 challenges folder/02_challenge.py
+++ 100 challenges folder/02_challenge.py
@@ -2,3 +2,32 @@
 Try to make the wordle game.
 """
 
+import random
+
+
+
+word_list = [
+    "apple", "grape", "flame", "chair", "stone", "water", "cloud", "tiger", "lions", "zebra",
+    "shine", "blink", "glove", "table", "piano", "brick", "mango", "pearl", "quick", "vocal",
+    "sword", "dream", "tease", "flash", "purse", "drink", "whale", "heist", "flick", "smile",
+    "judge", "guitar", "dance", "plane", "sword", "sleep", "cross", "haste", "night", "grass",
+    "storm", "piano", "crawl", "sneak", "brisk", "peace", "grove", "fetch", "blaze", "lunar"
+]
+
+
+random_word = random.choice(word_list)
+print(random_word)
+
+guess = 0
+attempt = 0
+
+while guess != random_word:
+
+    guess = input("Guess the wordle: ")
+    for char in guess:
+        if char in random_word:
+            print(f"{char} is in the wordle.")
+    attempt += 1
+else:
+
+
